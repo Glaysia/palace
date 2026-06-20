4ddbb5bacfcbf3eb68642670f3d0fba4fd6124bf
harrypc <harry261@naver.com>
2026-06-21 06:36:51 +0900

    Revert "Route terminal edge excitations through edge chains"
    This reverts commit 72857da1137984ca9333a971e033c7d6a4e54303.

--- palace/models/lumpedportoperator.cpp
+++ palace/models/lumpedportoperator.cpp
@@ -1532,10 +1532,6 @@ void LumpedPortOperator::AddExcitationBdrCoefficients(int excitation_idx,
     {
       continue;
     }
-    if (data.HasTerminalEdges())
-    {
-      continue;
-    }
     MFEM_VERIFY(std::abs(data.R) > 0.0,
                 "Unexpected zero resistance in excited lumped port!");
     for (std::size_t elem_idx = 0; elem_idx < data.elems.size(); elem_idx++)
@@ -1555,13 +1551,7 @@ void LumpedPortOperator::AddExcitationBdrCoefficients(int excitation_idx,
 
 bool LumpedPortOperator::HasTerminalEdgeExcitation(int excitation_idx) const
 {
-  for (const auto &[idx, data] : ports)
-  {
-    if (data.active && data.excitation == excitation_idx && data.HasTerminalEdges())
-    {
-      return true;
-    }
-  }
+  (void)excitation_idx;
   return false;
 }
 
